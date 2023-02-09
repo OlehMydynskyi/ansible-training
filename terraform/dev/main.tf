f6ea8970abf22d51425da157071cbb37715a63a6
OlehMydynskyi <www.medek@gmail.com>
2023-02-09 13:59:05 +0200

    dynamic inventory

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,15 +1,15 @@
 module instance1 {
   source = "../modules/EC2"
-  name          = "ubuntu1"
+  name          = "ubuntu"
   key_pair_name = "frankfurt-key"
   ami           = "ami-03e08697c325f02ab"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 }
 
 module instance2 {
   source = "../modules/EC2"
-  name          = "ubuntu2"
+  name          = "amazon"
   key_pair_name = "frankfurt-key"
-  ami           = "ami-03e08697c325f02ab"
-  instance_type = "t2.micro"
+  ami           = "ami-06c39ed6b42908a36"
+  instance_type = "t2.nano"
 }
